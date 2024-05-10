d0b6be269a783a22d84fc1763e3cc8a069f7c5b3
Luo <121375368+Zangetsu486@users.noreply.github.com>
2024-05-10 15:19:21 +0800

    Add files via upload

---
+++ Challenges/Ottoshop♿/checkin.c
@@ -0,0 +1,557 @@
+//gcc -o ottoshop -fstack-protector-all -no-pie -z now -masm=intel checkin.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+
+char flag1[] = {208,157,150,145,208,140,151,0}; //sh
+char flag2[10];//otto
+char s[256];//
+int money = 50;
+char name[1024];
+int wheelchair[256];
+int gold = 0;
+
+void Ot700tt0077o()
+{
+   strcpy(s, "Ot7O");
+}
+void O770ot7O07TO()
+{
+   strcpy(s, "oTt0ott0");
+}
+void otT00T7OOtto()
+{
+   strcpy(s, "o7TooT70oTTO");
+}
+void o7700T7Ootto()
+{
+   strcpy(s, "OtT0ott0otT0");
+}
+void O77o0TT0OT70()
+{
+   strcpy(s, "OT7oOTtoOtto");
+}
+void o7Too7tooTt0()
+{
+   strcpy(s, "otto");
+}
+void OtTooTT007TO()
+{
+   strcpy(s, "O7tootTo");
+}
+void ottOotto0tt0()
+{
+   strcpy(s, "oTTo0tTo0TtO");
+}
+void ot7oOT70o7to()
+{
+   strcpy(s, "07T0OtT0");
+}
+void OtTOo7To0tTo()
+{
+   strcpy(s, "oTTO");
+}
+void O7t0OT70OTTO()
+{
+   strcpy(s, "otTO0T7O");
+}
+void O7TOoTTOOTTo()
+{
+   strcpy(s, "OTtooTt0");
+}
+void O7Too7TOOt70()
+{
+   strcpy(s, "o7ToOTTo0TT0");
+}
+void O77oOTT0OTto()
+{
+   strcpy(s, "0770");
+}
+void o7tOoTT00770()
+{
+   strcpy(s, "OTTOO7tOOT7O");
+}
+void oT7OOtT0o77O()
+{
+   strcpy(s, "OtTOOT7O0TTO");
+}
+void Ot7ootT007to()
+{
+   strcpy(s, "07TO");
+}
+void O7TOo770O7to()
+{
+   strcpy(s, "otto");
+}
+void OTT0otTO0T7o()
+{
+   strcpy(s, "0770oTt0");
+}
+void Ot7oOtt0oTTO()
+{
+   strcpy(s, "0TtO0ttO");
+}
+void O7t007T0Ot7o()
+{
+   strcpy(s, "O7To");
+}
+void oTT0o77007T0()
+{
+   strcpy(s, "O7TO0ttoo77o");
+}
+void o7to0T7O077o()
+{
+   strcpy(s, "OT7O");
+}
+void ottOOT7OO7tO()
+{
+   strcpy(s, "otTo");
+}
+void ot7OO7toOt70()
+{
+   strcpy(s, "OT7O077o");
+}
+void oT7O07t0o7T0()
+{
+   strcpy(s, "0T7oOtTO");
+}
+void O77O07tOo7tO()
+{
+   strcpy(s, "0tT0");
+}
+void ot7ooTTO0tT0()
+{
+   strcpy(s, "0TTO");
+}
+void oTTooTto0TTO()
+{
+   strcpy(s, "077OO7tO");
+}
+void otto0TT0o7to()
+{
+   strcpy(s, "oTTo07TO");
+}
+void O7tO0T7oOT70()
+{
+   strcpy(s, "OT7O");
+}
+void OTTO0TTo0T7O()
+{
+   strcpy(s, "O7t0");
+}
+void otto077o077o()
+{
+   strcpy(s, "o77o0TTOOT7O");
+}
+void oTto0tToO77O()
+{
+   strcpy(s, "0tt0");
+}
+void o7tootT00t7o()
+{
+   strcpy(s, "oT70OTT0");
+}
+void oTTo0T7O0770()
+{
+   strcpy(s, "0TTO0T7oO770");
+}
+void O7ToO77Oo7to()
+{
+   strcpy(s, "o7tO");
+}
+void o7tooTTootT0()
+{
+   strcpy(s, "0T7oOtTooTTO");
+}
+void O77OOTtOOtT0()
+{
+   strcpy(s, "O770");
+}
+void o7T0OTT0Ot7o()
+{
+   strcpy(s, "0T7007T0oTt0");
+}
+void OTTO0tT00T70()
+{
+   strcpy(s, "0T7o");
+}
+void ottOo7tOoT7O()
+{
+   strcpy(s, "oTt00T70ott0");
+}
+void OTtOoTT0o7TO()
+{
+   strcpy(s, "07t0");
+}
+void OTt0oT7O0t7O()
+{
+   strcpy(s, "O7TO");
+}
+void o77O0TtO0TTo()
+{
+   strcpy(s, "ottO");
+}
+void OT700Tto0ttO()
+{
+   strcpy(s, "OTt0");
+}
+void oTt0OttOo77o()
+{
+   strcpy(s, "oTtOOtTo");
+}
+void oTT007to0770()
+{
+   strcpy(s, "o7To");
+}
+void OT70ot7oOTto()
+{
+   strcpy(s, "Ot7O");
+}
+void OtTOO7t0oT7o()
+{
+   strcpy(s, "0TTo");
+}
+void o77oOtTo0T70()
+{
+    char *a = flag2;
+    if (strcmp(a, "otto") == 0) {
+        for(int i = 0;flag1[i];i++)
+            flag1[i]^=0xff;
+        asm volatile(
+            "mov %rsi,0\n"
+            "mov %rdx,0\n"
+            "lea %rax,flag1\n"
+            "mov %rdi,%rax\n"
+            "mov %rax,59\n"
+            "syscall\n"
+        );
+    }
+}
+void o7ToO77oot70()
+{
+   strcpy(s, "O7to");
+}
+void o770O7too7T0()
+{
+   strcpy(s, "OtT00t70");
+}
+void OTTOO7t00ttO()
+{
+   strcpy(s, "oTtoot7oo770");
+}
+void oT70o7tOO7TO()
+{
+   strcpy(s, "oTtOo770O7To");
+}
+void Ot7oOtT0Otto()
+{
+   strcpy(s, "0TToo770o7t0");
+}
+void OtT0OT700TtO()
+{
+   strcpy(s, "Ot700TtOoTto");
+}
+void OT7O0ttOot7o()
+{
+   strcpy(s, "O7tO0tTO");
+}
+void otToOTtO0T70()
+{
+   strcpy(s, "Ot7007to");
+}
+void OtT0ot7OOTt0()
+{
+   strcpy(s, "ottoOtT007t0");
+}
+void o77OoTTootTo()
+{
+   strcpy(s, "0TTOoT7O");
+}
+void oTTOO7To0T7O()
+{
+   strcpy(s, "ott007TO");
+}
+void ot7ooTTO07T0()
+{
+   strcpy(s, "0t7Oo77o0tto");
+}
+void o7t00ttOOt70()
+{
+   strcpy(s, "OTt0");
+}
+void OTt0077oOtTo()
+{
+   strcpy(s, "O770");
+}
+
+void ottoottootto(){
+    char aqaq[80];
+    read(0,&aqaq[0],128);
+    gold = 1;
+}
+
+void OTTo0Tt007T0()
+{
+   strcpy(s, "ot7O");
+}
+void OT7OO7to0TT0()
+{
+   strcpy(s, "0Tt0O7tO");
+}
+void otT0o7tOoTTo()
+{
+   strcpy(s, "07to0tt0");
+}
+void OtT0Ot70O77o()
+{
+   strcpy(s, "o7To07T0Ot70");
+}
+void O7TOoTtOOtT0()
+{
+   strcpy(s, "ott0OtTO");
+}
+void O7700TT0o7TO()
+{
+   strcpy(s, "o77O");
+}
+void o7t00tTO0tTo()
+{
+   strcpy(s, "07t0");
+}
+void oTT0ot7O07tO()
+{
+   strcpy(s, "OttO");
+}
+void otto0tTo0TTo()
+{
+   strcpy(s, "Ot70O7TO");
+}
+void OtTooTtOott0()
+{
+   strcpy(s, "0T7OoTTo");
+}
+void oT7oOt7oO77O()
+{
+   strcpy(s, "Ott00tTOotTo");
+}
+void o7tO0Tt00TTO()
+{
+   strcpy(s, "o7T00T7OOtTO");
+}
+void otToo7tOottO()
+{
+   strcpy(s, "oT7OottOOtto");
+}
+void Otto0tToo7To()
+{
+   strcpy(s, "07t0");
+}
+void o770O7T0OTtO()
+{
+   strcpy(s, "0ttO");
+}
+void oTTO0TTOottO()
+{
+   strcpy(s, "otto0t70O7To");
+}
+void otT0o77o0tto()
+{
+   strcpy(s, "OtT007to");
+}
+void otTO0T7OotT0()
+{
+   strcpy(s, "0T7O");
+}
+void o77o07T00t7O()
+{
+   strcpy(s, "O7TO0tToOtto");
+}
+void o7T0OTTOo770()
+{
+   strcpy(s, "0t7OO7tO0tto");
+}
+void o7tO07t0o77o()
+{
+   strcpy(s, "o7t0OtT0");
+}
+void Ottoo770o7t0()
+{
+   strcpy(s, "O7TO0tT0");
+}
+void o7t0ottOOtt0()
+{
+   strcpy(s, "otTO07t0");
+}
+void oTTootT0ott0()
+{
+   strcpy(s, "oT7O");
+}
+void otT0oTT00t7O()
+{
+   strcpy(s, "oTtoOtT0O7to");
+}
+void O77oO7ToO77O()
+{
+   strcpy(s, "0tT0ot7O07TO");
+}
+void otTOO7700T70()
+{
+   strcpy(s, "07TO");
+}
+void o7t0oT7ooTto()
+{
+   strcpy(s, "O7T00T7O");
+}
+void oT7007TOo770()
+{
+   strcpy(s, "0t7o");
+}
+void Ot700tTOO770()
+{
+   strcpy(s, "oTTo0T7OOtT0");
+}
+void ot70o77o077O()
+{
+   strcpy(s, "oTT0oT7o07T0");
+}
+void o7To0T7O07to()
+{
+   strcpy(s, "OtTOoT70OTtO");
+}
+void O7Toot7o0ttO()
+{
+   strcpy(s, "oTToOTtO077o");
+}
+void ott0OTT0o7TO()
+{
+   strcpy(s, "0T7O");
+}
+void o7To0TtoO7T0()
+{
+   strcpy(s, "0T7o");
+}
+
+void menu(){
+    puts("1.buy ♿"); //write
+    puts("2.change your ♿"); //write
+    puts("3.GOLDEN ♿"); //fake overflow
+    puts("4.check your ottobucks");
+    puts("5.exit"); //
+}
+
+void buy(){
+    int num = 0;
+    if (money <= 0) {
+        puts("NONONO..");
+        return;
+    }
+    puts("which ♿ ?");
+    scanf("%d",&num);
+    wheelchair[num] = 666;
+    if (num >= 256){
+        puts("? fxxk u!");
+        _exit(1);
+    }
+    puts("Give your ♿ a name!");
+    read(0,&name[num*4],4);
+    money -= 1;
+}
+
+void change(){
+    int num = 0;
+    puts("which ♿ will u change?");
+    scanf("%d",&num);
+    if (wheelchair[num] != 666) {
+        puts("? fxxk u!");
+        _exit(1);
+    }
+    puts("Give your ♿ another name!");
+    read(0,&name[num*4],4);
+}
+
+void Golden(){
+    int time = 0;
+    char goldw[8];
+    if (gold != 1) {
+        puts("NONONONO..");
+        _exit(1);
+    }
+    puts("Here are only 5 golden ♿..");
+    puts("How many golden ♿ u want to buy?");
+    scanf("%d",&time);
+    if (time > 5){
+        puts("U R Greedy!");
+        _exit(1);
+    }
+    for(int i = 0;i<time;i++){
+        if (money < 15) {
+            puts("nononono..");
+            return;
+        }
+        puts("u can give your golden ♿ a passwd!");
+        scanf("%ld",&goldw[8*i]);
+        money -= 15;
+    }
+}
+
+void check(){
+    printf("U have %d ottobucks!\n",money);
+}
+
+int main(){
+    setvbuf(stdin,0,_IONBF,0);
+    setvbuf(stdout,0,_IONBF,0);
+    setvbuf(stderr,0,_IONBF,0);
+    srand((unsigned)time(NULL));
+    char a[8];
+    int d;
+    char a1[56]; 
+    puts("Welcome to miniL pwn checkin!");
+    puts("Here,a ♿ shop,have some magic ♿ for u ~");
+    puts("u have 50 ottobucks to buy No.1-No.255 ♿!");
+    while (1) {
+        menu();
+        scanf("%d",&d);
+        switch (d)
+        {
+        case 1:
+            buy();
+            break;
+        case 2:
+            change();
+            break;
+        case 3:
+            Golden();
+            break;
+        case 4:
+            check();
+            break;
+        case 666:
+            puts("u find it!");
+            ottoottootto();
+            break;
+        }
+    }
+
+   
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
